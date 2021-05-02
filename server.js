a10e9604cce0d8db21953c8bac22cdfe18557624
AndreasIrta <andreasirta0711@gmail.com>
2021-05-01 20:56:17 -0600

    add require on server.js

--- server.js
+++ server.js
@@ -2,4 +2,19 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const PORT = process.env.PORT || 8080; // application running port
+const PORT = process.env.PORT || 8080; 
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json()); 
+app.use(express.static(path.join(__dirname, "public")));
+
+// api routes get data, posting data and deleting data
+require('./routes/apiRoutes')(app);
+
+// html route for changing pages
+require('./routes/htmlRoutes')(app);
+
+// application request and response throught port
+app.listen(PORT,()=>{
+    console.log(`connected to port: ${PORT}`);
+});
