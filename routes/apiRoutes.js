9aada38236340bdba6d9a454c1e8483f91717957
AndreasIrta <andreasirta0711@gmail.com>
2021-05-01 22:14:34 -0600

    add promise function and app.get

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,7 @@ const path = require("path");
 let jsonDb = require("../db/db.json");
 
 module.exports = (app) => {
-    const readPrompt = () => 
+    const readPromise = () => 
     new Promise((res, rej) => {
         fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data)=> {
             if (err) rej (err);
@@ -13,3 +13,27 @@ module.exports = (app) => {
 }
 
 
+const writePromise = (data, operate) => 
+new Promises((res, rej) => {
+    let result;
+    switch(operate){
+        case 'post':
+            jsonDb.push(data);
+            break;
+        case 'delete':
+            jsonDb.splice(0,jsonDb.length);
+            jsonDb = data;
+            break;
+    }
+    fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify (jsonDb), (err) => {
+        if (err) rej (err);
+        result = (operate == 'post')?"<< WRITTEN !! >>":"<< DELETED !! >>"
+        res(result)
+    })
+})
+
+app.get("/api/notes", (req, res) => {
+    readPromise().then((result) => {
+        res.json(JSON.parse(result));
+    }).catch(err => console.log(err));
+});
